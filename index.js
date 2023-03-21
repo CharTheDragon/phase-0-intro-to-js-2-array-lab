fa7286e2245431833ef10d3c895aa6991d81d230
Nathaniel Swan <cygnet1.999@gmail.com>
2023-03-21 13:10:27 -0400

    functions are finished

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = [`Milo`, `Otis`, `Garfield`];
+
+function destructivelyAppendCat(Ralph){
+    cats.push(`Ralph`)
+};
+
+function destructivelyPrependCat(Bob){
+    cats.unshift(`Bob`)
+};
+
+function destructivelyRemoveLastCat(Ralph){
+    cats.pop()
+};
+
+function destructivelyRemoveFirstCat(Bob){
+    cats.shift()
+};
+
+function appendCat(Broom){ 
+    const moreCats = [...cats, `Broom`]
+    return moreCats
+};
+
+function prependCat(Arnold){
+    const beforeCats = [`Arnold`, ...cats]
+    return beforeCats
+}
+
+function removeLastCat(){
+    const byeGarfield = cats.slice(0,-1)
+    return byeGarfield
+}
+
+function removeFirstCat(){
+    const byeMilo = cats.slice(1,3)
+    return byeMilo
+}
